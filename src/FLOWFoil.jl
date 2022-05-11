60b0be83a5924af47f7fee271c1f4a79fb3b90ab
Judd Mehr <juddmehr@gmail.com>
2022-05-11 08:37:27 -0600

    start setting things up

--- src/FLOWFoil.jl
+++ src/FLOWFoil.jl
@@ -1,5 +1,34 @@
 module FLOWFoil
 
-# Write your package code here.
+# DEPENDENCIES
+using LinearAlgebra
+
+# EXPORTS
+
+# geometry types
+export Mesh, MeshSystem
+
+# geometry functions
+export generatemesh
+
+# common airfoil parameterizations
+export karmantrefftz, joukowsky
+
+# INCLUDED FILES
+
+# Types
+include("types.jl")
+
+# Geometry Generation and Modification
+include("geometry.jl")
+
+# Inviscid Multi-body Solver
+include("inviscid_analysis.jl")
+
+# Boundary Layer Integration
+include("viscous_analysis.jl")
+
+# Common Airfoil Parameterizations
+include("../common_parameterizations/conformal_mapping.jl")
 
 end
