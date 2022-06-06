5a95e6f7249888044c83437c7be37a7a908a7b84
Judd Mehr <juddmehr@gmail.com>
2022-06-06 14:38:53 -0600

    comment out viscous stuff for now

--- src/solve.jl
+++ src/solve.jl
@@ -101,82 +101,82 @@ function solve_inviscid_system(inviscidsystem, meshes; debug=false)
     return solution
 end
 
-###########################################
-#######      VISCOUS  PROBLEM       #######
-###########################################
+############################################
+########      VISCOUS  PROBLEM       #######
+############################################
 
-"""
-    solve_viscous(problem)
+#"""
+#    solve_viscous(problem)
 
-Solves the viscous problem.
+#Solves the viscous problem.
 
-**Arguments:**
-- 'problem::Problem' : Problem Definition.
+#**Arguments:**
+#- 'problem::Problem' : Problem Definition.
 
-**Returns:**
- - 'solution::ViscousSolution'
-"""
-function solve_viscous(problem; parameters=nothing)
+#**Returns:**
+# - 'solution::ViscousSolution'
+#"""
+#function solve_viscous(problem; parameters=nothing)
 
-    # Check to make sure you want the invsicid solution:
-    if !problem.viscous
-        @warn(
-            "Viscous mismatch, please set problem.viscous=true if you would like to solve the viscous problem."
-        )
-    end
+#    # Check to make sure you want the invsicid solution:
+#    if !problem.viscous
+#        @warn(
+#            "Viscous mismatch, please set problem.viscous=true if you would like to solve the viscous problem."
+#        )
+#    end
 
-    # initialize viscous solution
-    solution = initialize_viscous(problem)
+#    # initialize viscous solution
+#    solution = initialize_viscous(problem)
 
-    # solve coupled system
-    coupled_solve!(solution)
+#    # solve coupled system
+#    coupled_solve!(solution)
 
-    return solution
-end
+#    return solution
+#end
 
-"""
-    initalize_viscous(problem)
+#"""
+#    initalize_viscous(problem)
 
-Initialized viscous solution (solves invscid problem, initialized wake and boundary layer, etc.)
+#Initialized viscous solution (solves invscid problem, initialized wake and boundary layer, etc.)
 
-**Arguments:**
- - problem::Problem' : Problem to solve.
+#**Arguments:**
+# - problem::Problem' : Problem to solve.
 
-**Returns:**
- - solution::ViscousSolution' : Initialized ViscousSolution
-"""
-function initalize_viscous(problem; parameters=nothing)
+#**Returns:**
+# - solution::ViscousSolution' : Initialized ViscousSolution
+#"""
+#function initalize_viscous(problem; parameters=nothing)
 
-    # solve inviscid problem first
-    inviscid_solution = solve_inviscid(problem)
+#    # solve inviscid problem first
+#    inviscid_solution = solve_inviscid(problem)
 
-    # initialize parameters if needed
-    if parameters == nothing
-        parameters = defaultparameters(;
-            muinf=inviscid_solution.mesh.chord / problem.reynolds
-        )
-    end
+#    # initialize parameters if needed
+#    if parameters == nothing
+#        parameters = defaultparameters(;
+#            muinf=inviscid_solution.mesh.chord / problem.reynolds
+#        )
+#    end
 
-    # set thermodynamic properties TODO
-    properties = initialize_properties(problem, parameters, inviscid_solution)
+#    # set thermodynamic properties TODO
+#    properties = initialize_properties(problem, parameters, inviscid_solution)
 
-    # locate stagnation point TODO
+#    # locate stagnation point TODO
 
-    # split into pressure/suction sides TODO
+#    # split into pressure/suction sides TODO
 
-    # generate wake TODO
-    wake = generate_wake(problem, inviscid_solution)
+#    # generate wake TODO
+#    wake = generate_wake(problem, inviscid_solution)
 
-    # set dead air space behind blunt trailing edges. TODO
+#    # set dead air space behind blunt trailing edges. TODO
 
-    # initialize edge velocity TODO
+#    # initialize edge velocity TODO
 
-    # initialize boundary layer TODO
+#    # initialize boundary layer TODO
 
-    # update stagnation point location based on edge velocities of initialized boundary layer. TODO
+#    # update stagnation point location based on edge velocities of initialized boundary layer. TODO
 
-    # initialize viscous solution object TODO
-    solution = ViscousSolution()
+#    # initialize viscous solution object TODO
+#    solution = ViscousSolution()
 
-    return solution
-end
+#    return solution
+#end
