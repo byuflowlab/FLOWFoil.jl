72516c1e9a2a92da6ce83b0423906e501ba852e3
Documenter.jl <documenter@juliadocs.github.io>
2022-06-15 21:26:11 +0000

    build based on 633b470

--- dev/logo.jl
+++ dev/logo.jl
@@ -13,19 +13,19 @@ m2 = FLOWFoil.generate_mesh(x .+ 0.75, y .- 0.75)
 x, y = naca4(8.0, 5.0, 44.0)
 m3 = FLOWFoil.generate_mesh(x .- 0.9, y .- 0.75)
 
-inviscid_problem = FLOWFoil.Problem([m1; m3; m2 ], 4.0, 0.0; viscous=false)
+inviscid_problem = FLOWFoil.Problem([m1; m3; m2], 4.0, 0.0; viscous=false)
 inviscid_solution = FLOWFoil.solve(inviscid_problem)
 polar = FLOWFoil.inviscid_polar(inviscid_solution, 0.0)
 x, z, stream = FLOWFoil.calculate_stream_grid(
     inviscid_problem, inviscid_solution, [-1.0; 1.9], [-1.15; 0.65]
 )
 
-contour(x, z, stream, 25; linewidths=2, linestyles="-", colors=[(0.251, 0.388, 0.847)])
+contour(x, z, stream, 100; linewidths=2, linestyles="-", colors=[(0.251, 0.388, 0.847)])
 
 xu, xl, yu, yl = naca4(12.0, 5.0, 44.0; split=true)
 fill_between(xu, yu, reverse(yl); color=(0.22, 0.596, 0.149), zorder=2)
-plot(xu, yu; color=(0.22, 0.596, 0.149), zorder=3)
-plot(xl, yl; color=(0.22, 0.596, 0.149), zorder=3)
+plot(xu, yu; color=(0.22, 0.596, 0.149), linewidth=2, zorder=3)
+plot(xl, yl; color=(0.22, 0.596, 0.149), linewidth=2, zorder=3)
 
 xu, xl, yu, yl = naca4(12.0, 6.0, 44.0; split=true)
 xu .+= 0.75
@@ -33,8 +33,8 @@ xl .+= 0.75
 yu .-= 0.75
 yl .-= 0.75
 fill_between(xu, yu, reverse(yl); color=(0.584, 0.345, 0.698), zorder=2)
-plot(xu, yu; color=(0.584, 0.345, 0.698), zorder=3)
-plot(xl, yl; color=(0.584, 0.345, 0.698), zorder=3)
+plot(xu, yu; color=(0.584, 0.345, 0.698), linewidth=2, zorder=3)
+plot(xl, yl; color=(0.584, 0.345, 0.698), linewidth=2, zorder=3)
 
 xu, xl, yu, yl = naca4(8.0, 5.0, 44.0; split=true)
 xu .-= 0.9
@@ -42,10 +42,11 @@ xl .-= 0.9
 yu .-= 0.75
 yl .-= 0.75
 fill_between(xu, yu, reverse(yl); color=(0.796, 0.235, 0.2), zorder=2)
-plot(xu, yu; color=(0.796, 0.235, 0.2), zorder=3)
-plot(xl, yl; color=(0.796, 0.235, 0.2), zorder=3)
+plot(xu, yu; color=(0.796, 0.235, 0.2), linewidth=2, zorder=3)
+plot(xl, yl; color=(0.796, 0.235, 0.2), linewidth=2, zorder=3)
 
 axis("equal")
 axis("off")
 
 savefig("src/assets/logo.png"; bbox_inches="tight")
+# savefig("../test.pdf"; bbox_inches="tight")
