26f74a64c66e8e21ac304e6771f1db2e302d8ff0
juddmehr <juddmehr@gmail.com>
2025-02-21 14:34:58 -0700

    flip some buggy signs to get both cascade and planar methods working properly

--- src/martensen/singularities.jl
+++ src/martensen/singularities.jl
@@ -31,8 +31,8 @@ function calculate_periodic_vortex_influence(
     if pitch < eps()
         a = b = k = 0.0
     else
-        a = -(r_x * cos(stagger) - r_y * sin(stagger)) * 2.0 * pi / pitch
-        b = -(r_x * sin(stagger) + r_y * cos(stagger)) * 2.0 * pi / pitch
+        a = (r_x * cos(stagger) - r_y * sin(stagger)) * 2.0 * pi / pitch
+        b = (r_x * sin(stagger) + r_y * cos(stagger)) * 2.0 * pi / pitch
         cosha = 0.5 * (exp(a) + 1.0 / exp(a))
         k = 0.5 / pitch / (cosha - cos(b))
     end
@@ -61,10 +61,10 @@ function calculate_planar_vortex_influence(
     cosine_panel_angle_i = cos(stagger + paneli.panel_angle[i])
 
     # u = (y_j - y_i) / (r * 2 * pi)
-    u = system_geometry.r_y[i, j] / (2.0 * pi * system_geometry.r_squared[i, j])
+    u = -system_geometry.r_y[i, j] / (2.0 * pi * system_geometry.r_squared[i, j])
 
     # v = -(x_j - x_i) / (r * 2 * pi)
-    v = -system_geometry.r_x[i, j] / (2.0 * pi * system_geometry.r_squared[i, j])
+    v = system_geometry.r_x[i, j] / (2.0 * pi * system_geometry.r_squared[i, j])
 
     return (u * cosine_panel_angle_i + v * sine_panel_angle_i) * panelj.panel_length[j]
 end
@@ -72,5 +72,5 @@ end
 function calculate_periodic_self_vortex_influence(panel, i)
     #compute self-inducing coupling coefficients
     # return 0.5 - (panel.delta_angle[i] - pi) / (4.0 * pi)
-    return 0.5
+    return -0.5
 end
