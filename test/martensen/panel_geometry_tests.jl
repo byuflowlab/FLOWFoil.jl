48801882da13a270ebc7c81e0556682589188eca
ABEngineer2000 <jbenne26@byu.edu>
2025-02-22 12:08:18 -0700

    Completed and tested the System Geometry unit testing

--- test/martensen/panel_geometry_tests.jl
+++ test/martensen/panel_geometry_tests.jl
@@ -12,7 +12,7 @@
         panel_geometry_method,
         coordinates
     )
-    @test panels.npanels == 4
-    @test isapprox(panels.panel_angle, [-2.03444, -4.24874, 1.10715, -1.10715], atol = 0.001)
-    @test isapprox(panels.delta_angle, [-1.01722, 1.570795, 1.570795, -0.553575], atol = 0.001)
+    @test panels.npanels == 4 #test number of panels
+    @test isapprox(panels.panel_angle, [-2.03444, -4.24874, 1.10715, -1.10715], atol = 0.001) #test panel angle
+    @test isapprox(panels.delta_angle, [-1.01722, 1.570795, 1.570795, -0.553575], atol = 0.001) #test delta angle
 end
