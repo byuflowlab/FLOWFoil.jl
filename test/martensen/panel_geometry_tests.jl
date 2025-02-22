fa17f377180be7fa5bff1b7d6042fa26ea73f721
ABEngineer2000 <jbenne26@byu.edu>
2025-02-22 11:29:36 -0700

    Added unit test for Panel Geometry

--- test/martensen/panel_geometry_tests.jl
+++ test/martensen/panel_geometry_tests.jl
@@ -3,16 +3,16 @@
     #---------------------------------#
     #             PERIODIC            #
     #---------------------------------#
-    # Very Basic Test
-    x = [1.0; 2.0; 3.0; 4.0]
-    z = [0.0; 0.0; 0.0; 0.0]
+    # 4 panel diamond test
+    x = [1.0; 0.5; 0.0; 0.5; 1.0]
+    z = [0.0; -1.0; 0.0; 1.0; 0.0]
     coordinates = [x z]
-    panels = generate_panels(
-        PeriodicProblem(Vortex(Constant()), Neumann(), 1.0, 0.0), coordinates
+    panel_geometry_method = FLOWFoil.Martensen(false, 0.0, 0.0, 30.0, 100.0, false)
+    panels = FLOWFoil.generate_panel_geometry(
+        panel_geometry_method,
+        coordinates
     )
-    @test panels.npanels == 3
-    @test panels.panel_center == [1.5 0.0; 2.5 0.0; 3.5 0.0]
-    @test panels.panel_length == ones(3)
-    @test panels.panel_normal == [0.0 1.0; 0.0 1.0; 0.0 1.0]
-    @test panels.panel_angle == zeros(3)
+    @test panels.npanels == 4
+    @test isapprox(panels.panel_angle, [-2.03444, -4.24874, 1.10715, -1.10715], atol = 0.001)
+    @test isapprox(panels.delta_angle, [-1.01722, 1.570795, 1.570795, -0.553575], atol = 0.001)
 end
