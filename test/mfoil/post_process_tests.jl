8b941c602c63dba61ef3bcdbd1ace83f725a8989
ABEngineer2000 <jbenne26@byu.edu>
2025-05-07 15:26:21 -0600

    Fixed mfoil post_process_tests.jl

--- test/mfoil/post_process_tests.jl
+++ test/mfoil/post_process_tests.jl
@@ -2,16 +2,16 @@
 
     # - Very Basic Test - #
 
-    x, z = x, z = FLOWFoil.naca4()
+    x, z = AirfoilTools.naca4()
     coordinates = [x z]
     outputs = analyze(coordinates, [0.0; 5.0]; method=Mfoil())
 
-    Xfoil.set_coordinates(reverse(x), reverse(z))
-    cl0, cm0 = Xfoil.solve_alpha(0.0)
-    cl5, cm5 = Xfoil.solve_alpha(5.0)
+    set_coordinates(reverse(x), reverse(z))
+    cl0, cm0 = solve_alpha(0.0)
+    cl5, cm5 = solve_alpha(5.0)
 
-    @test isapprox(outputs.lift[1], cl0, atol=1e-5)
-    @test isapprox(outputs.moment[1], cm0, atol=1e-3)
-    @test isapprox(outputs.lift[2], cl5, atol=1e-5)
-    @test isapprox(outputs.moment[2], cm5, atol=1e-3)
+    @test isapprox(outputs.cl[1], cl0, atol=1e-5)
+    @test isapprox(outputs.cm[1], cm0, atol=1e-3)
+    @test isapprox(outputs.cl[2], cl5, atol=1e-5)
+    @test isapprox(outputs.cm[2], cm5, atol=1e-3)
 end
