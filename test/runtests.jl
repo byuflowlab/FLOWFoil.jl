6a4f9c013d9ad76a5cf232b4629263f96748c3d8
nlehnhof <n.lehnhof01@gmail.com>
2025-06-05 14:49:06 -0600

    updated directory info for tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,35 +8,35 @@ using FiniteDiff
 using ImplicitAD
 import NeuralFoil as nf
 
-# @info("Hess Smith Tests")
-# dir = testdir = joinpath(@__DIR__, "hess_smith")
-# for file in filter(f -> endswith(f, "tests.jl"), readdir(dir))
-#     include(joinpath(dir, file))
-# end
+@info("Hess Smith Tests")
+dir = joinpath(@__DIR__, "hess_smith")
+for file in filter(f -> endswith(f, "tests.jl"), readdir(dir))
+    include(joinpath(dir, file))
+end
 
-# @info("Axisymmetric Panel Method Tests")
-# dir = testdir = joinpath(@__DIR__, "lewis")
-# for file in filter(f -> endswith(f, "tests.jl"), readdir(dir))
-#     include(joinpath(dir, file))
-# end
+@info("Axisymmetric Panel Method Tests")
+dir = joinpath(@__DIR__, "lewis")
+for file in filter(f -> endswith(f, "tests.jl"), readdir(dir))
+    include(joinpath(dir, file))
+end
 
-# @info("Periodic Panel Method Tests")
-# dir = testdir = joinpath(@__DIR__, "martensen")
-# for file in filter(f -> endswith(f, "tests.jl"), readdir(dir))
-#     include(joinpath(dir, file))
-# end
+@info("Periodic Panel Method Tests")
+dir = joinpath(@__DIR__, "martensen")
+for file in filter(f -> endswith(f, "tests.jl"), readdir(dir))
+    include(joinpath(dir, file))
+end
 
-# @info("Inviscid Mfoil Tests")
-# dir = testdir = joinpath(@__DIR__, "mfoil")
-# for file in filter(f -> endswith(f, "tests.jl"), readdir(dir))
-#     include(joinpath(dir, file))
-# end
+@info("Inviscid Mfoil Tests")
+dir = joinpath(@__DIR__, "mfoil")
+for file in filter(f -> endswith(f, "tests.jl"), readdir(dir))
+    include(joinpath(dir, file))
+end
 
-# @info("NeuralFoil Tests")
-# dir = joinpath(@__DIR__, "neural_foil")
-# for file in filter(f -> endswith(f, "tests.jl"), readdir(dir))
-#     include(joinpath(dir, file))
-# end
+@info("NeuralFoil Tests")
+dir = joinpath(@__DIR__, "neural_foil")
+for file in filter(f -> endswith(f, "tests.jl"), readdir(dir))
+    include(joinpath(dir, file))
+end
 
 @info("LegacyXfoil Tests")
 dir = joinpath(@__DIR__, "legacy_xfoil")
